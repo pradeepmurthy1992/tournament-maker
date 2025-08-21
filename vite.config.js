a9d01f6bd856a7f71a36090205e17c4523f4b08e
pradeepmurthy1992 <pradeepmurthy8@gmail.com>
2025-08-21 12:09:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANT: if your repo is NOT named "tournament-maker",
-// change the base to '/<your-repo-name>/'
 export default defineConfig({
   plugins: [react()],
   base: '/tournament-maker/',
+  define: { 'process.env': {} } // <-- add this line
 })
