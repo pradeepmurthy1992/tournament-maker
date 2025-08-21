a39c8629ab95423a45a57754b2011375a9b705d3
pradeepmurthy1992 <pradeepmurthy8@gmail.com>
2025-08-21 12:17:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tournament-maker/',
-  define: { 'process.env': {} } // <-- add this line
+  base: '/tournament-maker/',   // <-- if your repo name differs, update this
+  define: { 'process.env': {} } // helps some xlsx env warnings
 })
