c09a62d155cd1c1bf287f16a05c9f56ab3307cfe
pradeepmurthy1992 <pradeepmurthy8@gmail.com>
2025-08-28 16:18:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
+// vite.config.js (ESM)
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// ⚠️ CHANGE repoName to your repo name exactly
+const repoName = "tournament-maker"; // <-- e.g., "my-repo"
+
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [react()],
 });
